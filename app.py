05be73c4e30c73175ad3270149ba1ed63856c27e
Aadit-Ambadkar <aadit.ambadkar@gmail.com>
2022-08-06 17:14:20 -0700

    Initial Commits

---
+++ app.py
@@ -0,0 +1,11 @@
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
+SLACK_APP_TOKEN = os.environ['SLACK_APP_TOKEN']
+
+app = App(token=SLACK_BOT_TOKEN)
+
